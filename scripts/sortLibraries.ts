0d70b59cd99dd40d72decc9f0f7f3754ede5b58a
Kamil Pajdzik <pajdziupaj@gmail.com>
2023-06-09 21:47:02 -0700

    Sort libraries

---
+++ scripts/sortLibraries.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env esrun
+import { LibrarySummary } from "./libraryTypes.js";
+import { readLibraryDataFile, writeLibraryDataFile } from "./files.js";
+
+const sortLibrary = (library: LibrarySummary, otherLibrary: LibrarySummary) => {
+  if (library.name < otherLibrary.name) {
+    return -1;
+  }
+
+  if (library.name === otherLibrary.name) {
+    return 0;
+  }
+
+  return 1;
+};
+
+const main = async () => {
+  const libraries = await readLibraryDataFile<LibrarySummary[]>(
+    "../assets/libraries.json"
+  );
+  const sortedLibraries = libraries.sort(sortLibrary);
+  await writeLibraryDataFile("../assets/libraries2.json", sortedLibraries);
+};
+
+await main();
