ae41012d421496c5436523fff75e34c1495d1bf3
Kamil Pajdzik <pajdziupaj@gmail.com>
2023-06-17 12:12:12 -0700

    Add logging to storage

--- src/storage.ts
+++ src/storage.ts
@@ -1,24 +1,46 @@
 import browser from "webextension-polyfill";
 
 import { LibrarySummary } from "./libraryTypes.js";
+import { logDebug, logError, logWarn } from "./debug.js";
 
 const runsInAddon = () => typeof window === "undefined" || !window.browser;
 const LIBRARY_KEY = "aulible-library-key";
 
 export const saveLibrary = async (library: LibrarySummary): Promise<void> => {
-  if (runsInAddon()) {
-    await browser.storage.sync.set({ LIBRARY_KEY: library });
-  } else {
-    localStorage.setItem(LIBRARY_KEY, JSON.stringify(library));
+  try {
+    logDebug(`Saving library: ${library.name}`);
+
+    if (runsInAddon()) {
+      logDebug("Saving library in addon");
+      await browser.storage.sync.set({ LIBRARY_KEY: library });
+    } else {
+      logWarn("Saving library in local storage");
+      localStorage.setItem(LIBRARY_KEY, JSON.stringify(library));
+    }
+
+    logDebug("Library saved");
+  } catch (e) {
+    logError(`Error saving library: ${e}`);
   }
 };
 
 export const loadLibrary = async (): Promise<LibrarySummary | undefined> => {
-  if (runsInAddon()) {
-    const libraryKey = await browser.storage.sync.get(LIBRARY_KEY);
-    return libraryKey[LIBRARY_KEY];
-  }
+  try {
+    logDebug("Loading library");
+    let library: LibrarySummary | undefined;
 
-  const libraryKey = localStorage.getItem(LIBRARY_KEY);
-  return libraryKey ? JSON.parse(libraryKey) : undefined;
+    if (runsInAddon()) {
+      logDebug("Loading library from addon");
+      library = (await browser.storage.local.get([LIBRARY_KEY])) as LibrarySummary;
+    } else {
+      logWarn("Loading library from local storage");
+      const data = localStorage.getItem(LIBRARY_KEY);
+      library = data ? JSON.parse(data) : undefined;
+    }
+
+    logDebug(`Loaded library: ${JSON.stringify(library, undefined, 2)}`);
+    return library;
+  } catch (e) {
+    logError(`Error loading library: ${e}`);
+  }
 };
